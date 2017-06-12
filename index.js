dc6e48cd99ce5b660e2a2bb53e679a2432d8c0cb
Jonathan Gray <jonathanceddy@gmail.com>
2017-06-12 14:17:45 -0700

    exporting new modules

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 export {default as Drawer} from 'react-native-drawer'
-export {default as ImageZoom} from 'react-native-image-zoom'
+// export {default as ImageZoom} from 'react-native-image-zoom'
 export {default as Modal} from 'react-native-modal'
+export {default as ModalDatepicker} from 'react-native-modal-datepicker'
+export {default as ExternalLink} from 'react-native-external-link'
